b11acfebfcfacfbb31fadd15d15f02395fa4eaa7
zhhyang <zhhyang0404@gmail.com>
2017-12-06 12:48:44 +0800

    add auth

--- src/config.js
+++ src/config.js
@@ -3,11 +3,11 @@
  */
 module.exports = {
   development: {
-    mongodbURI: 'mongodb://39.106.33.48:27017/quiz',
+    mongodbURI: 'mongodb://quiz:budee123@39.106.33.48:27017/quiz',
     port: 5000,
   },
   production: {
-    mongodbURI: 'mongodb://39.106.33.48:27017/quiz',
+    mongodbURI: 'mongodb://quiz:budee123@39.106.33.48:27017/quiz',
     port: 8080,
   }
 }
